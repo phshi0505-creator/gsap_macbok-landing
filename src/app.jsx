7da02d378a084263a079b3169a4ce1a1293318af
Phoenix Shi <phoen@pp-macbook.local>
2025-12-14 20:42:24 +1100

    Initial Commit

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from "./components/navbar.jsx";
+
+const app = () => {
+  return (
+    <main>
+      <Navbar />
+
+    </main>
+  );
+};
+
+export default app;
